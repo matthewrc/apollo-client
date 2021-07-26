fd7934d59b2cdd1d3bd877743e602b28ce09e110
Brian Kim <briankimpossible@gmail.com>
2021-07-26 14:54:19 -0400

    fix missing paren

--- src/react/hooks/__tests__/useQuery.test.tsx
+++ src/react/hooks/__tests__/useQuery.test.tsx
@@ -1107,7 +1107,7 @@ describe('useQuery Hook', () => {
         updateQuery: (prev, { fetchMoreResult }) => ({
           letters: prev.letters.concat(fetchMoreResult.letters),
         }),
-      });
+      }));
 
       await waitForNextUpdate();
       expect(result.current.loading).toBe(false);
